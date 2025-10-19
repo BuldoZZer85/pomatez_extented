52b1b39a84ea4369c5c4f9c23173b6d96de29bd3
BuldoZZer85 <114488285+BuldoZZer85@users.noreply.github.com>
2025-10-19 11:11:30 +0500

    Update typings for IPC methods

--- app/renderer/src/typings.d.ts
+++ app/renderer/src/typings.d.ts
@@ -1,8 +1,22 @@
-declare module "*.woff";
-declare module "*.woff2";
-declare module "*.png";
-declare module "*.jpg";
-declare module "*.wav";
-declare module "*.mp4";
-declare module "*.ogv";
-declare module "*.webm";
+declare module '*.mp3' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.wav' {
+  const src: string;
+  export default src;
+}
+
+// Расширяем типы для window.ipcRenderer
+declare global {
+  interface Window {
+    ipcRenderer: {
+      send: (channel: string, data?: any) => void;
+      invoke: (channel: string, data?: any) => Promise<any>;
+      on: (channel: string, func: (...args: any[]) => void) => void;
+      removeListener: (channel: string, func: (...args: any[]) => void) => void;
+      removeAllListeners: (channel: string) => void;
+    };
+  }
+}
